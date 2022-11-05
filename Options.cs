aa8b8bf6501f8cca7df986224a31e59362362e37
Abdallah Darwish <abdallah.darwish1@outlook.com>
2022-11-05 22:49:17 +0400

    Upgrade

--- Options.cs
+++ Options.cs
@@ -1,17 +1,16 @@
 using System.Collections.Generic;
 using CommandLine;
 
-namespace EncodingFixer
+namespace EncodingFixer;
+
+public class Options
 {
-    public class Options
-    {
-        [Option('f', "files", Required = true, HelpText = "Names (not paths) of text files to fix their encoding, you can use patterns like *.srt, seperate files by a comma(,).", Separator = ',')]
-        public IEnumerable<string> InputFiles { get; set; }
+    [Option('f', "files", Required = true, HelpText = "Names (not paths) of text files to fix their encoding, you can use patterns like *.srt, seperate files by a comma(,).", Separator = ',')]
+    public IEnumerable<string> InputFiles { get; set; }
 
-        [Option('d', "directory", Required = false, HelpText = "Parent directory of input files, if ignored it will be working directory.")]
-        public string? Directory { get; set; }
+    [Option('d', "directory", Required = false, HelpText = "Parent directory of input files, if ignored it will be working directory.")]
+    public string? Directory { get; set; }
 
-        [Option('v', Default = false, HelpText = "Prints all debugging messages to standard output.")]
-        public bool Verbose { get; set; }
-    }
+    [Option('v', Default = false, HelpText = "Prints all debugging messages to standard output.")]
+    public bool Verbose { get; set; }
 }
--- Program.cs
+++ Program.cs
@@ -3,101 +3,106 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using CommandLine;
+using CommandLine.Text;
 using Console = Colorful.Console;
-namespace EncodingFixer
+namespace EncodingFixer;
+
+class Program
 {
-    class Program
+    public static void PrintError(string error, bool exit = false)
     {
-        public static void PrintError(string error)
+        Console.WriteLine(error, Color.Red);
+        if (exit)
         {
-            Console.WriteLine(error, Color.Red);
-        }
-        private static bool verbose = false;
-        public static void Log(string msg)
-        {
-            if (!verbose) { return; }
-            Console.WriteLine(msg, Color.Blue);
-        }
-
-        public static void Main(string[] args)
-        {
-            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-
-            Parser.Default.ParseArguments<Options>(args)
-              .WithParsed(opts => RunOptionsAndReturnExitCode(opts))
-              .WithNotParsed((errs) => HandleParseError(errs));
-        }
-
-        private static void HandleParseError(IEnumerable<Error> errs)
-        {
-            errs = errs.Where(e => e.Tag != ErrorType.HelpRequestedError);
-            if (!errs.Any())
-            {
-                return;
-            }
-            PrintError("One or more error happened during the parsing of the arguments.\nErros: ");
-            foreach (var err in errs.Where(e => e.Tag != ErrorType.HelpRequestedError))
-            {
-                Console.WriteLine(err.Tag);
-            }
             Environment.Exit(-1);
         }
+    }
 
-        private static void RunOptionsAndReturnExitCode(Options opts)
+    private static bool _verbose = false;
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static void Log(string msg)
+    {
+        if (!_verbose) { return; }
+        Console.WriteLine(msg, Color.Blue);
+    }
+
+    public static void Main(string[] args)
+    {
+        Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+        Parser.Default.ParseArguments<Options>(args)
+          .WithParsed(opts => RunOptionsAndReturnExitCode(opts))
+          .WithNotParsed((errs) => HandleParseError(errs));
+    }
+
+    private static void HandleParseError(IEnumerable<Error> errs)
+    {
+        errs = errs.Where(e => e.Tag != ErrorType.HelpRequestedError);
+        if (!errs.Any())
         {
-            verbose = opts.Verbose;
+            return;
+        }
+        PrintError("One or more error happened during the parsing of the arguments.\nErros: ");
+        foreach (var err in errs)
+        {
+            Console.WriteLine(err.Tag);
+        }
+        Environment.Exit(-1);
+    }
+
+    private static void RunOptionsAndReturnExitCode(Options opts)
+    {
+        _verbose = opts.Verbose;
+        try
+        {
+            if (opts.Directory != null && !Directory.Exists(opts.Directory))
+            {
+                PrintError("Invalid files directory.", true);
+            }
+        }
+        catch
+        {
+            PrintError("Invalid files directory.", true);
+        }
+
+        Log("Building file list.");
+        List<string> files = new(1000);
+        var searchDir = new DirectoryInfo(opts.Directory ?? Environment.CurrentDirectory);
+        Log($"Searching for files in {searchDir}");
+
+        foreach (var pattern in opts.InputFiles)
+        {
             try
             {
-                if (opts.Directory != null && !Directory.Exists(opts.Directory))
+                if (File.Exists(pattern))
                 {
-                    PrintError("Invalid files directory.");
-                    Environment.Exit(-1);
+                    files.Add(pattern);
+                    continue;
                 }
             }
-            catch
-            {
-                PrintError("Invalid files directory.");
-                Environment.Exit(-1);
-            }
-
-            Log("Building file list.");
-            List<string> files = new(1000);
-            var searchDir = new DirectoryInfo(opts.Directory ?? Environment.CurrentDirectory);
-            Log($"Searching for files in {searchDir}");
-
-            foreach (var pattern in opts.InputFiles)
-            {
-                try
-                {
-                    if (File.Exists(pattern))
-                    {
-                        files.Add(pattern);
-                        continue;
-                    }
-                }
-                catch { }
-                var searchResult = searchDir
+            catch { }
+            var searchResult = searchDir
                 .EnumerateFiles(pattern, SearchOption.TopDirectoryOnly)
                 .Select(f => f.FullName);
-                if (!searchResult.Any())
-                {
-                    PrintError($"File: {pattern} doesn't exist in directory.");
-                }
-                files.AddRange(searchResult);
-            }
-            Log($"List of files to process:\n\t{string.Join($"{Environment.NewLine}\t", files)}");
-            Encoding fileOriginalEncoding = Encoding.GetEncoding(1256);
-            Encoding fileNewEncoding = new UTF8Encoding();
-            Parallel.ForEach(files, file =>
+            if (!searchResult.Any())
             {
-                Log($"Processing file {file}");
-                File.WriteAllText(file, File.ReadAllText(file, fileOriginalEncoding), fileNewEncoding);
-            });
-            Console.WriteLine("Fixed encoding of all files!", Color.Green);
-            Environment.Exit(0);
+                PrintError($"Pattern: {pattern} didn't match any files in directory.");
+            }
+            files.AddRange(searchResult);
         }
+        Log($"List of files to process:\n\t{string.Join($"{Environment.NewLine}\t", files)}");
+        Encoding fileOriginalEncoding = Encoding.GetEncoding(1256);
+        Encoding fileNewEncoding = new UTF8Encoding();
+        Parallel.ForEach(files, file =>
+        {
+            Log($"Processing file {file}");
+            File.WriteAllText(file, File.ReadAllText(file, fileOriginalEncoding), fileNewEncoding);
+        });
+        Console.WriteLine("Fixed encoding of all files!", Color.Green);
+        Environment.Exit(0);
     }
 }
